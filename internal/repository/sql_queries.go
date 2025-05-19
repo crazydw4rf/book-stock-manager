43f47343827a2f500bd3f99018dd3307e58042fa
Binar Nugroho <binarnugroho775@gmail.com>
2025-05-19 21:06:44 +0700

    Rename book table to books and update SQL queries

--- internal/repository/sql_queries.go
+++ internal/repository/sql_queries.go
@@ -1,13 +1,13 @@
 package repository
 
 const (
-	bookCreate        = `INSERT INTO book(book_id,isbn,title,author,publisher,published_at,stock) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *`
-	bookGetById       = `SELECT * FROM book WHERE book_id = $1 LIMIT 1`
-	bookGetByISBN     = `SELECT * FROM book WHERE isbn = $1 LIMIT 1`
-	bookGetBooksMany  = `SELECT * FROM book OFFSET $1 LIMIT $2`
-	bookDelete        = `DELETE FROM book WHERE book_id = $1 RETURNING book_id`
-	bookGetTotalCount = `SELECT COUNT(*) FROM book`
-	bookUpdate        = `UPDATE book SET
+	bookCreate        = `INSERT INTO books(book_id,isbn,title,author,publisher,published_at,stock) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *`
+	bookGetById       = `SELECT * FROM books WHERE book_id = $1 LIMIT 1`
+	bookGetByISBN     = `SELECT * FROM books WHERE isbn = $1 LIMIT 1`
+	bookGetBooksMany  = `SELECT * FROM books OFFSET $1 LIMIT $2`
+	bookDelete        = `DELETE FROM books WHERE book_id = $1 RETURNING book_id`
+	bookGetTotalCount = `SELECT COUNT(*) FROM books`
+	bookUpdate        = `UPDATE books SET
 isbn = COALESCE(NULLIF($2, ''), isbn),
 title = COALESCE(NULLIF($3, ''), title),
 author = COALESCE(NULLIF($4, ''), author),
